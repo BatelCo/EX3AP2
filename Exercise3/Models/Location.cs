248528d06a47d3e245b2f305fb2065dd7865f7d8
BATEL <batel.cohen100@gmail.com>
2019-05-30 12:52:22 +0300

    30/5 12:52

---
+++ Exercise3/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercise3.Models
+{
+    public class Location
+    {
+        public string Xcordinate { get; set; }
+        public string Ycordinate { get; set; }
+
+
+
+    }
+}
