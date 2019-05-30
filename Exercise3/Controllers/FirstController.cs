1bc6e0a86ef7a7bfbfb33ff48ce338d2f085a7d6
BATEL <batel.cohen100@gmail.com>
2019-05-30 12:03:53 +0300

    30/5 12:03

---
+++ Exercise3/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercise3.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        // return web page
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // only with post call 
+        //[HttpPost]
+        //public ActionResult IndexPost() {
+
+        // }
+
+    }
+}
