4977a69d257bd0038e0f1a688577f827d60c1e60
Yoni Segal <yonisegal13@gmail.com>
2019-06-02 16:45:46 +0300

    Almost finished task 2 (sort of)!

--- Exercise3/App_Start/RouteConfig.cs
+++ Exercise3/App_Start/RouteConfig.cs
@@ -20,7 +20,6 @@ namespace Exercise3
             url: "display/flight1/4",
             defaults: new { controller = "Fourth", action = "Display4" }
          );
-
             // first route : display/127.0.0.1/5400
             routes.MapRoute(
             name: "Display",
@@ -39,8 +38,6 @@ namespace Exercise3
             url: "save/{ip}/{port}/4/10/flight1",
             defaults: new { controller = "Third", action = "Display3" }
           );
-
-
             // default
           routes.MapRoute(
                 name: "Default",
@@ -48,12 +45,6 @@ namespace Exercise3
                 // if you dont get enything go hear
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute(
-                name: "Four",
-                url: "display/{ip}/{port}/4",
-                defaults: new { controller = "First", action = "Four" }
-                );
         }
     }
 }
--- Exercise3/Controllers/SecondController.cs
+++ Exercise3/Controllers/SecondController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Exercise3.Models;
 
 namespace Exercise3.Controllers
 {
@@ -18,12 +21,12 @@ namespace Exercise3.Controllers
         // the matching view - Display2
         public ActionResult Display2(string ip, int port)
         {
-        //        InfoModel.Instance.close_client();
-        //        InfoModel.Instance.connect_client(ip, port);
-        //        ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
-        //        ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
-               return View();
+           
+            InfoModel.Instance.close_client();
+            InfoModel.Instance.connect_client(ip, port);
+            ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
+            ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
+            return View();
         }
-
     }
 }
