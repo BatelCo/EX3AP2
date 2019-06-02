a5926678b0df3f2afc907623158a0cb80a23d3ee
BATEL <batel.cohen100@gmail.com>
2019-06-02 16:46:21 +0300

    AA

--- Exercise3/Controllers/SecondController.cs
+++ Exercise3/Controllers/SecondController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Exercise3.Models;
 
 namespace Exercise3.Controllers
 {
@@ -18,11 +19,17 @@ namespace Exercise3.Controllers
         // the matching view - Display2
         public ActionResult Display2(string ip, int port)
         {
-        //        InfoModel.Instance.close_client();
-        //        InfoModel.Instance.connect_client(ip, port);
-        //        ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
-        //        ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
-               return View();
+
+         
+            InfoModel.Instance.close_client();
+            InfoModel.Instance.connect_client(ip, port);
+            ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
+            ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
+
+            
+            // ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
+            // ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
+            return View();
         }
 
     }
