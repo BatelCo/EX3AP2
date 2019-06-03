1075af5b67dafb3289f6669c51fcb7b616ef8d85
BATEL <batel.cohen100@gmail.com>
2019-06-03 13:08:57 +0300

    שש

--- Exercise3/Controllers/thirdController.cs
+++ Exercise3/Controllers/thirdController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Exercise3.Models;
 
 namespace Exercise3.Controllers
 {
@@ -17,28 +15,17 @@ namespace Exercise3.Controllers
         }
 
         // the matching view - Display3
-        public ActionResult Display3(string ip, int port, int freq, int time)
+        public ActionResult Display3(string ip, int port)
         {
-            InfoModel.Instance.close_client();
-            InfoModel.Instance.connect_client(ip, port);
-            ViewBag.freq = freq;
-            ViewBag.time = time;
-            ViewBag.lat = double.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
-            ViewBag.lon = double.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
-            ViewBag.throttle = double.Parse(InfoModel.Instance.Read("get /controls/engines/current-engine/throttle\r\n"));
-            ViewBag.rudder = double.Parse(InfoModel.Instance.Read("get /controls/flight/rudder\r\n"));
-            WriteToFile(ViewBag.lat, ViewBag.lon, ViewBag.throttle, ViewBag.rudder);
+            //        InfoModel.Instance.close_client();
+            //        InfoModel.Instance.connect_client(ip, port);
+            //        ViewBag.lat = float.Parse(InfoModel.Instance.Read("get /position/latitude-deg\r\n"));
+            //        ViewBag.lon = float.Parse(InfoModel.Instance.Read("get /position/longitude-deg\r\n"));
+            //        ViewBag.throttle = float.Parse(InfoModel.Instance.Read("get /controls/engines/current-engine/throttle\r\n"));
+            //        ViewBag.rudder = float.Parse(InfoModel.Instance.Read("get /controls/flight/rudder\r\n"));
+            //private readonly string rudderStr = "get /controls/flight/rudder\r\n";
             return View();
         }
 
-        public void WriteToFile(double lat, double lon, double throttle, double rudder)
-        {
-            using (StreamWriter writetext = new StreamWriter("C:/Users/yonis/Desktop/Computer Science/Coding2/Exercise3/Exercise3/App_Data/data.txt", true))
-            {
-                writetext.WriteLine(lat + ";" + lon + ";" + throttle + ";" + rudder);
-                writetext.AutoFlush = true;
-                writetext.Close();
-            }           
-        }
     }
 }
